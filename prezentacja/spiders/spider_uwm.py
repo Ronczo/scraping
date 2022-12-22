f3e9018efbfa060334da072ce8afc425227d3eea
Marcin Jarosiński <marcin.jarosinski@stxnext.pl>
2022-12-22 15:31:23 +0100

    start

--- prezentacja/spiders/spider_uwm.py
+++ prezentacja/spiders/spider_uwm.py
@@ -2,44 +2,29 @@ import scrapy
 
 
 class SpiderUWM(scrapy.Spider):
-    name = "spider-uwm"
+    name = "spider-uwm-test"
     start_url = "http://wmii.uwm.edu.pl/ogloszenia"
+    no_allowed_chars = [".pdf", ".docx", "mailto", ".."]
 
-    def start_requests(self):
-        yield scrapy.Request(url=self.start_url, callback=self.parse)
+    # def start_requests(self):
 
-    def parse(self, response):
-        all_links = response.css("div.view-content a")
-        for link in all_links:
-            url = link.css("::attr(href)").get()
-            if self._is_valid_url(url):
-                yield response.follow(url=url, callback=self.scrape_announcement)
-        # paginacja
-        if next_page_url := response.css(
-            "a[title='Przejdź do następnej strony']::attr(href)"
-        ).get():
-            yield response.follow(url=next_page_url, callback=self.parse)
+    # def parse():
 
-    def scrape_announcement(self, response):
-        title = response.css("h1.page-title::text").get()
-        # texts_list = response.css("div.node__content ::text").getall()
-        # content = "\n".join(texts_list)
-        content = response.xpath(
-            "string(//div[contains(@class, 'node__content')])"
-        ).get()
-        source_url = response.url
-        yield {
-            "title": title,
-            "content": content,
-            "source_url": source_url,
-        }
+    # def scrape_announcement(self, response):
 
-    @staticmethod
-    def _is_valid_url(url):
+    # yield {
+    #     "title": title,
+    #     "content": content,
+    #     "source_url": source_url,
+    # }
+
+    def _is_valid_url(self, url):
         if url:
-            no_allowed_chars = [".pdf", ".docx", "mailto"]
-            is_not_document = not any(char in url for char in no_allowed_chars)
-            proper_domain = True
-            if url.startswith("http") and not url.startswith("http://wmii.uwm.edu.pl"):
-                proper_domain = False
-            return all([is_not_document, proper_domain])
+            is_not_document = not any(char in url for char in self.no_allowed_chars)
+            proper_domain = (
+                True
+                if url.startswith("http")
+                and not url.startswith("http://wmii.uwm.edu.pl")
+                else False
+            )
+            return is_not_document and proper_domain
